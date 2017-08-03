d25990ba149ddfcbf38738307ccfebe26bb48a76
THEredhoodieguy <sonicfan321@gmail.com>
2017-08-03 15:45:47 -0400

    Got rid of self reference to yvel in Zombie class. Coding is still hard:

--- agents.py
+++ agents.py
@@ -78,7 +78,7 @@ class Zombie(object):
         if(self.x + xvel > xbound or self.x + xvel < 0):
             new_x = self.x - xvel
         if(self.y + yvel > ybound or self.y + yvel < 0):
-            new_y = self.y - self.yvel
+            new_y = self.y - yvel
         self.x = new_x
         self.y = new_y
 
